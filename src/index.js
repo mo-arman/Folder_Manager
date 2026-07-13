03596f8d7eb15ad6ca4296bc3f2214d60747b526
Mohammad Arman <moharman3818@gmail.com>
2026-07-13 19:45:54 +0530

    Bill Manager App

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BillManager from './BillManager';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BillManager />
+  </React.StrictMode>
+);
