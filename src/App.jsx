3c73c57d7897badc315181993100ee2f22080aa0
Mohammad Arman <161627043+mo-arman@users.noreply.github.com>
2026-07-15 13:14:41 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BillManager from './BillManager';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BillManager />
+    </div>
+  );
+}
+
+export default App;
